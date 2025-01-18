e25191d408c782e74a27c0a041090d198b141998
Oyame Ogbeche <oyogbeche@gmail.com>
2025-01-18 16:52:10 +0100

    feat: add configurations files to reflect the new architecture
    added configuration files for ses, s3, api gateway, and cloudfront to reflect the new architecture for a web UI.
    Removed configurations for sns after switching to ses for the new use case

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,44 +1,20 @@
-variable "sns_topic_name" {
-  type = string
-}
-
 variable "tags" {
   type = map(string)
-}
-
-variable "display_name" {
-  type = string
-}
-
-variable "subscriptions" {
-  description = <<EOT
-A list of subscriptions for the SNS topic. Each subscription is an object with the following attributes:
-  - protocol: The protocol of the subscription (e.g., email, sms, lambda, etc.).
-  - endpoint: The endpoint for the subscription (e.g., email address, phone number, etc.).
-  - raw_message_delivery: (Optional) Whether raw message delivery is enabled.
-EOT
-  type = list(object({
-    protocol             = string
-    endpoint             = string
-    raw_message_delivery = optional(bool, false)
-  }))
-  default = []
-}
-
-variable "email" {
-  type = string
-}
-
-variable "phone_number" {
-  type = string
 }
 
 variable "function_name" {
   type = string
 }
 
-variable "iam_policy" {
-  type = string
+variable "gd_lambda_policies" {
+  type = map(object({
+    Version = string
+    Statement = list(object({
+      Effect   = string
+      Action   = list(string)
+      Resource = list(string)
+    }))
+  }))
 }
 
 variable "runtime" {
@@ -79,4 +55,105 @@ variable "targets" {
   type = list(object({
     arn = string
   }))
+}
+
+variable "table_name" {
+  type = string
+}
+
+variable "hash_key" {
+  type = string
+}
+
+variable "hash_key_type" {
+  type = string
+}
+
+variable "gp_function_name" {
+  type = string
+}
+
+variable "game_preference_handler" {
+  type = string
+}
+
+variable "gp_lambda_policies" {
+  description = "A map of policies to attach to the Lambda role"
+  type = map(object({
+    Version = string
+    Statement = list(object({
+      Effect   = string
+      Action   = list(string)
+      Resource = list(string)
+    }))
+  }))
+}
+
+variable "gp_lambda_source_path" {
+  type = string
+}
+
+variable "gp_zip_output_path" {
+  type = string
+}
+
+variable "gp_environment_variables" {
+  type = map(string)
+}
+
+variable "api_name" {
+  type = string
+}
+
+variable "protocol_type" {
+  type = string
+}
+
+variable "auto_deploy" {
+  type = bool
+}
+
+variable "stage_name" {
+  type = string
+}
+
+variable "api_gw_lambda_permissions" {
+  type = list(object({
+    function_name = string
+  }))
+}
+
+variable "routes" {
+  type = map(object({
+    route_key          = string
+    target             = optional(string)
+    integration_type   = optional(string)
+    integration_method = optional(string)
+    integration_uri    = optional(string)
+    authorization_type = optional(string)
+  }))
+}
+
+variable "email_identity" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "bucket_policy" {
+  type = string
+}
+
+variable "origin_domain_name" {
+  type = string
+}
+
+variable "origin_ssl_protocols" {
+  type = list(string)
+}
+
+variable "origin_id" {
+  type = string
 }
