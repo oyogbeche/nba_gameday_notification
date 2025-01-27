3e1948a87aa011c8551a63b7da33e6128bee922c
Oyame Ogbeche <oyogbeche@gmail.com>
2025-01-27 17:23:54 +0100

    refactor: add remote state for state files"

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.82.2"
     }
   }
+  backend "s3" {
+    bucket         = "oyogbeche-resume-terraform-state"
+    key            = "nba_gameday/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
